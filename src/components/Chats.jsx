1cfcf25af5d73006e9279a8d2ae7094f7667f9bf
Akiyoshi <zundokokiyoshi999@gmail.com>
2021-03-10 22:33:38 +0900

    Material-UIのカスタマイズ

--- src/components/Chats.jsx
+++ src/components/Chats.jsx
@@ -1,15 +1,15 @@
 import React from "react"
-import { createStyles, Theme, makeStyles } from '@material-ui/core/styles';
+import { createStyles, makeStyles } from '@material-ui/core/styles';
 import List from '@material-ui/core/List';
 import {Chat} from "./index"
 
-const useStyles = makeStyles((theme: Theme) =>
+const useStyles = makeStyles(() =>
   createStyles({
-    root: {
-      width: '100%',
-      maxWidth: '36ch',
-      backgroundColor: theme.palette.background.paper,
-    },
+    "chats":{
+      height: 400,
+      padding: "0",
+      overflow: "auto"
+    }
   }),
 );
 
@@ -17,7 +17,7 @@ const Chats = (props) => {
     const classes = useStyles();
 
     return(
-        <List className={classes.root}>
+        <List className={classes.chats}>
             {props.chats.map((chat, index) =>{
                 return <Chat text={chat.text} type={chat.type} key={index.toString()}/>
             })}
