cc247b9a28fbf4e00e68f668c35424c2f9b84342
Akiyoshi <zundokokiyoshi999@gmail.com>
2021-03-11 22:31:08 +0900

    ライフサイクルの活用

--- src/App.jsx
+++ src/App.jsx
@@ -35,7 +35,13 @@ export default class App extends React.Component {
   selectAnswer = (selectAnswer, nextQuestionId) => {
     switch(true){
       case (nextQuestionId === 'init'):
-        this.displayNextQuestion(nextQuestionId)
+        setTimeout(() => this.displayNextQuestion(nextQuestionId),500);
+        break;
+      case(/^https:*/.test(nextQuestionId)):
+        const a = document.createElement('a')
+        a.href = nextQuestionId;
+        a.target = '_blank';
+        a.click()
         break;
       default:
         const chat = {
@@ -50,7 +56,7 @@ export default class App extends React.Component {
           chats: chats
         })
 
-        this.displayNextQuestion(nextQuestionId);
+        setTimeout(() => this.displayNextQuestion(nextQuestionId),500);
         break;
     }
   }
@@ -82,6 +88,13 @@ export default class App extends React.Component {
   componentDidMount(){
     const initAnswer = "";
     this.selectAnswer(initAnswer, this.state.currentId)
+  }
+
+  componentDidUpdate(){
+    const scrollArea = document.getElementById("scroll-area");
+    if (scrollArea){
+      scrollArea.scrollTop = scrollArea.scrollHeight
+    }
   }
 
   render() {
